d35f7552e733f1eabfbe37f446f66e67f894f97f
qietingfengyinzhou <33718246+qietingfengyinzhou@users.noreply.github.com>
2023-06-13 11:08:52 +0800

    Update SuperappApplicationTests.java

--- src/test/java/com/alipay/cloudrun/superapp/SuperappApplicationTests.java
+++ src/test/java/com/alipay/cloudrun/superapp/SuperappApplicationTests.java
@@ -1,13 +1,7 @@
 package com.alipay.cloudrun.superapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SuperappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
